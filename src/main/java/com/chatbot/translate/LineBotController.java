3a1716ea02550e95073bdc396c9126df1f25d0b3
Vincentjs20 <vincentyoelsinatra2@gmail.com>
2018-11-17 15:14:01 +0700

    cobain databse4

--- src/main/java/com/chatbot/translate/LineBotController.java
+++ src/main/java/com/chatbot/translate/LineBotController.java
@@ -156,54 +156,12 @@ public class LineBotController
         return new ResponseEntity<String>(HttpStatus.OK);
     }
 
-    private void translate(String fromLang, String toLang, String text, String payload) throws IOException {
-        // TODO: Should have used a 3rd party library to make a JSON string from an object
-        String jsonPayload = new StringBuilder()
-                .append("{")
-                .append("\"fromLang\":\"")
-                .append(fromLang)
-                .append("\",")
-                .append("\"toLang\":\"")
-                .append(toLang)
-                .append("\",")
-                .append("\"text\":\"")
-                .append(text)
-                .append("\"")
-                .append("}")
-                .toString();
-
-        //URL url = new URL(ENDPOINT);
-//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//        conn.setDoOutput(true);
-//        conn.setRequestMethod("POST");
-//        conn.setRequestProperty("X-WM-CLIENT-ID", CLIENT_ID);
-//        conn.setRequestProperty("X-WM-CLIENT-SECRET", CLIENT_SECRET);
-//        conn.setRequestProperty("Content-Type", "application/json");
-//
-//        OutputStream os = conn.getOutputStream();
-//        os.write(jsonPayload.getBytes());
-//        os.flush();
-//        os.close();
-//
-//        int statusCode = conn.getResponseCode();
-//        System.out.println("Status Code: " + statusCode);
-//        BufferedReader br = new BufferedReader(new InputStreamReader(
-//                (statusCode == 200) ? conn.getInputStream() : conn.getErrorStream()
-//        ));
-//        String output;
-//        while ((output = br.readLine()) != null) {
-//            replyToUser(payload, output);
-//            //System.out.println(output);
-//        }
-//        conn.disconnect();
-    }
-
     private void simpanPesan(String perintah, Payload payload) {
         String[] data = perintah.split(" ");
         String id = payload.events[0].source.userId;
         String key = data[1] + id;
         String value = data[2];
-
+        hmap.put(key, value);
     }
 //    public void insertData(Simpanan simpanan){
 //        String sql="INSERT INTO simpanan(id_person,key,value)"
